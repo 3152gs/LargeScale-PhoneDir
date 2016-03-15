2103657bc2a96705a42d00f87adaac46f831cead
Gauri <gauri.s.gautam@gmail.com>
2016-03-15 00:14:11 -0400

    Program Done

--- src/main/java/PhoneDirectory.java
+++ src/main/java/PhoneDirectory.java
@@ -3,67 +3,96 @@ import java.util.*;
 
 public class PhoneDirectory {
 	
-	String get_info;      //to return getNumber(name)
+	String get_info, location_File, name, number;    
+		
+	//Construcor
 	
-	public void addEntry(String name, String phone_number) throws IOException{
+		HashMap<String,String> Phone_hashMap= new HashMap<String,String>();
 		
-		Properties property = new Properties();
-		OutputStream outfile = new FileOutputStream("src/main/resources/PhoneDirectory.txt", true);
+		public PhoneDirectory(String location) throws IOException
+		{
 		
-		property.setProperty(name, phone_number);
-		
-		property.store(outfile, null);  //store value to the directory
-		outfile.close();
+			location_File= location;
+			
+			
+			File fileName = new File(location);
+			String line=null;
+			try{
+				FileReader fileReader = new FileReader(fileName);			
+				BufferedReader bufferedReader = new BufferedReader(fileReader);	
+				while((line=bufferedReader.readLine())!=null){
+					String[] value = line.split(" ");
+					name=value[0];
+					number=value[1];
+					Phone_hashMap.put(name,number);
+				}
+				bufferedReader.close();
+			}
+			
+			catch(FileNotFoundException ex){
+				System.out.println("Unable to open file");
+			}
+			catch(IOException ex){
+				System.out.println("File not opened.");
+			}
+		}
+	
+//Add a new entry
+	public void addEntry(String name,String number) throws IOException{
+		Phone_hashMap.put(name,number);
 	}
 	
-	public void deleteEntry(String name) throws IOException{
+//Delete an entry
+	public void deleteEntry(String name) {
 		
-		Properties property = new Properties();
-		
-		InputStream infile = new FileInputStream("src/main/resources/PhoneDirectory.txt");   //delete the contents
-		property.load(infile);
-		property.remove(name);
-		infile.close();
-		
-		OutputStream outfile = new FileOutputStream("src/main/resources/PhoneDirectory.txt"); //after deletion
-		property.store(outfile, null);
-		outfile.close();
-		
+		if(Phone_hashMap.get(name)!=null){
+			Phone_hashMap.remove(name);
+			}		
+		else{
+			System.out.println("Name not found");
+			}
 	}
+	
 	
 //Input a person's name and get phone number
 	public String getNumber(String name) throws IOException{
-		
-		Properties property = new Properties();
-		InputStream infile = new FileInputStream("src/main/resources/PhoneDirectory.txt");
-		property.load(infile);
-		get_info = property.getProperty(name);
-		infile.close();
-		
-		System.out.println(get_info);
-		return get_info;
-		
+		String phoneNumber = Phone_hashMap.get(name);
+		return phoneNumber;		
 	}
 	
 //Change the phone number of people	
-	public void changeEntry(String name, String phone_number) throws IOException{
-		
-		Properties property = new Properties();
-		
-		InputStream infile = new FileInputStream("src/main/resources/PhoneDirectory.txt");
-		property.load(infile);
-		property.replace(name, phone_number);
-		infile.close();
-		
-		OutputStream outfile = new FileOutputStream("src/main/resources/PhoneDirectory.txt"); 
-		property.store(outfile, null);
-		outfile.close();
-	}
+	public void changeEntry(String name, String number){
+		if(Phone_hashMap.get(name)==null){
+			System.out.println("Name not found.");
+		}
+		else{
+			Phone_hashMap.put(name,number);
+		}
 	
-	//Construcor
-	
-	public PhoneDirectory(String location_File) throws IOException
-	{
-		
 	}
+
+//Write into the same file
+	public void write(){
+		File fileObject = new File(location_File);
+		Iterator iter = Phone_hashMap.entrySet().iterator();
+		try{
+			FileWriter fileWriting = new FileWriter(fileObject);
+			BufferedWriter bufferWriting = new BufferedWriter (fileWriting);
+			while(iter.hasNext()){
+				HashMap.Entry pair = (HashMap.Entry)iter.next();
+				bufferWriting.write(pair.getKey()+""+ pair.getValue());
+				bufferWriting.newLine();
+			}
+		}
+	catch(FileNotFoundException ex){
+		System.out.println("File not found");
+	}
+	catch(IOException ex){
+		System.out.println("File not found");
+	}
+
 }
+}
+
+
+
